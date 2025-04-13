cc0f5a71ba92b0df0c09cd419a139f6f11d0b11d
Barnabas Gonda <barnabasgonda@Barnabass-iMac.local>
2025-04-13 17:17:13 +0200

    post test

--- forum_app/tests.py
+++ forum_app/tests.py
@@ -1,6 +1,6 @@
 from django.urls import reverse
 from rest_framework import status
-from rest_framework.test import APITestCase
+from rest_framework.test import APITestCase, APIClient
 from django.contrib.auth.models import User
 from .models import Question
 from .api.serializers import QuestionSerializer
@@ -20,6 +20,21 @@ class QuestionTests(APITestCase):
     def setUp(self):
         self.user = User.objects.create_user(username='testuser', password='testpassword')
         self.question = Question.objects.create(title='Test Question', content='Test Content', author=self.user, category='frontend')
+        self.client = APIClient()
+        self.client.login(username='testuser', password='testpassword')
+    
+    def test_list_post_question(self):
+        url = reverse('question-list')
+        data = {
+            'title':'Question1',
+            'content':'1Content',
+            'author':self.user.id,
+            'category':'frontend',
+        }
+       
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    
         
     def test_detail_question(self):
         url = reverse('question-detail', kwargs={'pk': self.question.id})
@@ -28,3 +43,6 @@ class QuestionTests(APITestCase):
        
         expected_data = QuestionSerializer(self.question).data
         self.assertEqual(response.data, expected_data)
+        self.assertDictEqual(response.data, expected_data)
+        self.assertJSONEqual(response.content, expected_data)
+        self.assertContains(response, 'title')
